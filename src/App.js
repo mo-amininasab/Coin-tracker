e9cec6ede47aa07d5db113a0d0206d2817c3fefd
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-08-16 13:39:13 +0430

    tailwind, axios installed.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+// api
+import { fetchCoins } from "./api/index";
 
 function App() {
+  const [coins, setCoins] = useState("");
+
+  useEffect(() => {
+    const fetchCoinsFromAPI = async () => {
+      setCoins(await fetchCoins());
+    };
+
+    fetchCoinsFromAPI();
+  }, []);
+
+  console.log(coins);
+
   return (
-    <div className="App">
+    <div className="text-red-500">
+      <div>
+        <h1>Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search"></input>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const url = "https://api.coingecko.com/api/v3";
+
+export const fetchCoins = async () => {
+  try {
+    const { data } = await axios.get(
+      `${url}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+    );
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
