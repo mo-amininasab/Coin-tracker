fd8116bdc9c7fe6a39f02e94b2b9f98b65910f83
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-08-16 14:42:30 +0430

    first look completed.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import React, { useState, useEffect } from "react";
 // api
 import { fetchCoins } from "./api/index";
 
+// components
+import Coin from "./components/Coin";
+
 function App() {
   const [coins, setCoins] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchCoinsFromAPI = async () => {
@@ -13,16 +17,40 @@ function App() {
 
     fetchCoinsFromAPI();
   }, []);
+  console.log('coins',coins);
 
-  console.log(coins);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = Object.values(coins).filter((coin) =>
+    coin.name.trim().toLowerCase().includes(search.trim().toLowerCase())
+  );
 
   return (
-    <div className="text-red-500">
+    <div>
       <div>
-        <h1>Search a currency</h1>
+        <h1 className="text-white">Search a currency</h1>
         <form>
-          <input type="text" placeholder="Search"></input>
+          <input
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={handleChange}
+          ></input>
         </form>
+        {filteredCoins.map((coin) => (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.total_volume}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.market_cap}
+          ></Coin>
+        ))}
       </div>
     </div>
   );
--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Coin() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Coin({ image, name, symbol, price, volume, priceChange, marketcap }) {
+  return (
+    <div className="flex space-x-5 mb-6 pb-4 ml-24 border-b-2 text-left text-white">
+      <img src={image} alt="crypto" className="w-8 h-8 mr-4"/>
+      <h1 className="w-32">{name}</h1>
+      <p className="w-14 uppercase">{symbol}</p>
+
+      <p className="w-24">${price}</p>
+      <p className="w-32">${volume.toLocaleString()}</p>
+      {priceChange < 0 ? <p className="text-red-800 w-16">{priceChange.toFixed(2)}%</p>: <p className="text-green-800 w-16">{priceChange.toFixed(2)}%</p>}
+      <p>Mkt Cap: ${marketcap.toLocaleString()}</p>
+    </div>
+  );
 }
 
-export default Coin
+export default Coin;
